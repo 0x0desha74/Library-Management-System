bd497b0abe3f422404f047047e62f821196550aa
0x0desha74 <mudesha74@gmail.com>
2025-03-05 22:42:25 +0200

    Implement  UnitOfWork

--- Repositories/UnitOfWork.cs
+++ Repositories/UnitOfWork.cs
@@ -1,6 +1,41 @@
-﻿namespace Bookly.APIs.Repositories
+﻿using Bookly.APIs.Data;
+using Bookly.APIs.Entities;
+using Bookly.APIs.Interfaces;
+using System.Collections;
+
+namespace Bookly.APIs.Repositories
 {
-    public class UnitOfWork
+    public class UnitOfWork : IUnitOfWork
     {
+        private readonly ApplicationDbContext _context;
+        private Hashtable _repositories;
+        public UnitOfWork(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IGenericRepository<TEntity> Repository<TEntity>() where TEntity : BaseEntity
+        {
+            if (_repositories is null)
+                _repositories = new Hashtable();
+            var type = typeof(TEntity).Name;
+            if (!_repositories.ContainsKey(type))
+            {
+                var repository = new GenericRepository<TEntity>(_context);
+                _repositories.Add(type, repository);
+            }
+            return _repositories[type] as GenericRepository<TEntity>;
+        }
+        public async Task<int> Complete()
+        {
+           return await _context.SaveChangesAsync();
+        }
+
+        public void Dispose()
+        {
+           _context.Dispose();
+        }
+
+       
     }
 }
