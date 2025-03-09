f67fd232e7c0e8dbb3afed12ca0c6c4622dd981a
0x0desha74 <mudesha74@gmail.com>
2025-03-09 06:39:47 +0200

    Register endpoint

--- Controllers/AccountsController.cs
+++ Controllers/AccountsController.cs
@@ -7,7 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Bookly.APIs.Controllers
 {
-   
+
     public class AccountsController : BaseApiController
     {
         private readonly UserManager<AppUser> _userManager;
@@ -24,7 +24,7 @@ namespace Bookly.APIs.Controllers
         {
             var user = await _userManager.FindByEmailAsync(model.Email);
             if (user is null) return Unauthorized(new ApiResponse(401));
-            var result = await _signInManager.CheckPasswordSignInAsync(user, model.Password,false);
+            var result = await _signInManager.CheckPasswordSignInAsync(user, model.Password, false);
             if (!result.Succeeded) return Unauthorized(new ApiResponse(401));
             return Ok(new UserDto()
             {
@@ -33,8 +33,43 @@ namespace Bookly.APIs.Controllers
                 Token = "Token"
             });
 
+        }
+        [HttpPost("register")]
+        public async Task<ActionResult<UserDto>> Register(RegisterDto model)
+        {
+            if (CheckEmailExists(model.Email).Result.Value)
+                return BadRequest(new ApiResponse(400, "Email Already Exists"));
+
+            var user = new AppUser()
+            {
+                DisplayName = model.DisplayName,
+                Email = model.Email,
+                PhoneNumber = model.PhoneNumber,
+                UserName = model.Email.Split("@")[0]
+            };
+            var result = await _userManager.CreateAsync(user, model.Password);
+            if (!result.Succeeded) return BadRequest(new ApiResponse(400));
+            return Ok(new UserDto()
+            {
+                DisplayName= user.DisplayName,
+                Email=user.Email,
+                Token = "Token"
+            });
+
+
 
         }
+
+
+
+
+        [HttpGet("emailexists")]
+        public async Task<ActionResult<bool>> CheckEmailExists(string email)
+        {
+            return await _userManager.FindByEmailAsync(email) is not null;
+        }
+
+
 
     }
 }
