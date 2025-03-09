7d87849d0315dfe8fc7f8cd428572dc37d9fe5e6
0x0desha74 <mudesha74@gmail.com>
2025-03-09 05:40:25 +0200

    Login Endpoint

---
+++ Controllers/AccountsController.cs
@@ -0,0 +1,40 @@
+﻿using Bookly.APIs.DTOs;
+using Bookly.APIs.Entities;
+using Bookly.APIs.Error;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace Bookly.APIs.Controllers
+{
+   
+    public class AccountsController : BaseApiController
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signInManager;
+
+        public AccountsController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<UserDto>> Login(LoginDto model)
+        {
+            var user = await _userManager.FindByEmailAsync(model.Email);
+            if (user is null) return Unauthorized(new ApiResponse(401));
+            var result = await _signInManager.CheckPasswordSignInAsync(user, model.Password,false);
+            if (!result.Succeeded) return Unauthorized(new ApiResponse(401));
+            return Ok(new UserDto()
+            {
+                DisplayName = user.DisplayName,
+                Email = user.Email,
+                Token = "Token"
+            });
+
+
+        }
+
+    }
+}
